4f6bc9e47f1e73d21b20f4eef98e789da5214cbe
Callum <github@calobyte.net>
2025-03-19 13:08:59 +0000

    updates

--- 41.sh
+++ 41.sh
@@ -21,18 +21,18 @@ clear
 echo "----------------------------------------------------"
 echo "Welcome to Nubuntu $OS_VERSION (v$VERSION)"
 echo "=> The following will be installed:"
-echo " -> debs: $debs"
+echo " -> rpms: $rpms"
 echo " -> flatpaks: $flatpaks"
-if [ -n "$apt_install" ]; then
+if [ -n "$dnf_install" ]; then
   echo "=> the following apt install(s) will be invoked"
-  echo " -> $apt_install"
+  echo " -> $dnf_install"
 fi
-if [ -n "$apt_remove" ]; then
+if [ -n "$dnf_remove" ]; then
   echo "=> the following apt remove(s) will be invoked"
-  echo " -> $apt_remove"
+  echo " -> $dnf_remove"
 fi
 if [[ $debloat == "yes" ]]; then
-  echo "=> snap packages will be removed"
+  # echo "=> snap packages will be removed"
 fi
 if [[ $neaten == "yes" ]]; then
   echo "=> the shell will also be neatened"
@@ -46,20 +46,19 @@ echo "----------------------------------------------------"
 echo "*****************************************************"
 echo "Upgrading and Updating Installing and Removing"
 echo "*****************************************************"
-sudo apt update
-sudo apt upgrade -yq
+sudo dnf update -yq
 
-if [ -n "$apt_install" ]; then
-  IFS=',' read -ra app_list <<< "$apt_install"
+if [ -n "$dnf_install" ]; then
+  IFS=',' read -ra app_list <<< "$dnf_install"
   for app in "${app_list[@]}"; do
-      sudo apt install -yq $app
+      sudo dnf install -yq $app
   done
 fi
 
-if [ -n "$apt_remove" ]; then
-  IFS=',' read -ra app_list <<< "$apt_remove"
+if [ -n "$dnf_remove" ]; then
+  IFS=',' read -ra app_list <<< "$dnf_remove"
   for app in "${app_list[@]}"; do
-      sudo apt remove -yq $app
+      sudo dnf remove -yq $app
   done
 fi
 
@@ -69,83 +68,50 @@ if [[ $debloat == "yes" ]]; then
   echo "Debloating"
   echo "*****************************************************"
 
-  echo "*****************************************************"
-  echo "Removing Snaps and snapd"
-  echo "*****************************************************"
-
-  MAX_TRIES=30
-
-  for try in $(seq 1 $MAX_TRIES); do
-    INSTALLED_SNAPS=$(snap list 2> /dev/null | grep -c  ^Name || true)
-    if (( $INSTALLED_SNAPS == 0 )); then
-      echo "all snaps removed"
-    fi
-    echo "Attempt $try of $MAX_TRIES to remove $INSTALLED_SNAPS snaps."
-
-    snap list 2> /dev/null | grep -v ^Name |  awk '{ print $1 }'  | xargs -r -n1  sudo snap remove || true
-  done
-
-  sudo apt autoremove -yq --purge snapd
-  sudo apt-mark hold snapd
-  sudo rm -rf /snap
-  sudo rm -rf $HOME/snap
-  sudo rm -rf /root/snap
-
-  echo "*****************************************************"
-  echo "Snaps removed"
-  echo "*****************************************************"
-
-  sudo apt -yq remove gnome-user-docs yelp
+  sudo dnf remove -yq gnome-user-docs yelp
 
 fi
 
 
 echo "*****************************************************"
-echo "Installing essential deb applications"
+echo "Installing essential RPM applications"
 echo "*****************************************************"
 mkdir $DOWNLOAD_PATH
 
 # INSTALL: VS CODE
 if [[ $debs =~ "vscode" ]]; then
-  echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
-  sudo apt-get install -yq wget gpg
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-  rm -f packages.microsoft.gpg
-  sudo apt install -yq apt-transport-https
-  sudo apt update
-  sudo apt install -yq code
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+  sudo dnf check-update
+  sudo dnf install -yq code
 fi
 
 # INSTALL: Chrome
 if [[ $debs =~ "chrome" ]]; then
-  wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DOWNLOAD_PATH/chrome.deb
-  sudo apt install -yq $DOWNLOAD_PATH/chrome.deb
+  sudo dnf install -yq google-chrome-stable
 fi
 
 # INSTALL: dbeaver
 if [[ $debs =~ "dbeaver" ]]; then
-  wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O $DOWNLOAD_PATH/dbeaver.deb
-  sudo apt install -yq $DOWNLOAD_PATH/dbeaver.deb
+  wget -c https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm -O $DOWNLOAD_PATH/dbeaver.rpm
+  sudo dnf install -yq $DOWNLOAD_PATH/dbeaver.rpm
 fi
 
 # INSTALL: docker
 if [[ $debs =~ "docker" ]]; then
-  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-  sudo apt-get update
-  sudo apt-get install -yq ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  sudo apt-get update
-  sudo apt-get -yq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf remove -yq docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+  sudo dnf -yq install dnf-plugins-core
+  sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+  sudo dnf install -yq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo usermod -aG docker $USER
 fi
 
